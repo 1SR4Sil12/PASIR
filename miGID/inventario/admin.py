744a51b1cf376792af7a70cb4672084ecd708f16
Israel Silva <escorpion45678@gmail.com>
2023-10-18 12:59:58 +0200

    Commit 1
    Modelos hechos a falta de confirmación. Hacerle las vistas para que tenga funcionalidad y meterle urls bien

--- miGID/inventario/admin.py
+++ miGID/inventario/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Sede, Articulo
+
+admin.site.register(Sede)
+admin.site.register(Articulo)
--- miGID/inventario/models.py
+++ miGID/inventario/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Sede(models.Model):
+	nom = models.CharField('Nombre', max_length=50)
+	cod = models.CharField('Código', max_length=3)
+
+class Articulo(models.Model):
+	sede = models.ForeignKey(Sede, on_delete=models.CASCADE)
+	nom = models.CharField('Nombre', max_length=50)
+	cod = models.CharField('Código', max_length=9)
+	des = models.TextField('Descripción')
+	pvp = models.IntegerField('Precio')
+
+	def __str__(self):
+		return f'{self.nom} {self.pvp} €'
+
+	class Meta:
+		verbose_name_plural = "Artículos"
+
+class Perfil(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE, default=0)
+	ape = models.CharField('Apellidos', max_length=50)
+
+	def __str__(self):
+		return f'{self.user} {self.ape}'
+
+	class Meta:
+		verbose_name_plural = "Perfiles"
